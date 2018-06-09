a2a6226e80312f9666bafe1bde98aa7a8d851ffe
ASHLEY BREUNICH <ashleybreunich@gmail.com>
2018-06-09 10:29:44 -0700

    Got the graph working

--- app.js
+++ app.js
@@ -89,10 +89,10 @@ function handleClick(event) {
   if(Product.totalClicks > 24) {
     Product.container.removeEventListener('click', handleClick);
     //show the list after the last click
-    // pushFinalTally();
+    pushFinalTally();
     showTally();
     console.log(Product.FinalViews);
-    // renderChart();
+    renderChart();
   }
 
   //start to add up the total clicks
@@ -108,11 +108,11 @@ function handleClick(event) {
 }
 displayPics();
 
-// function pushFinalTally() {
-//   for(var i = 0; i < Product.all.length; i++){
-//     Product.FinalViews.push(Product.all[i].votes);
-//   }
-// }
+function pushFinalTally() {
+  for(var i = 0; i < Product.all.length; i++){
+    Product.FinalViews.push(Product.all[i].votes);
+  }
+}
 
 //show the tally using the list in the DOM once the event listener has been removed
 function showTally() {
@@ -124,46 +124,46 @@ function showTally() {
   }
 }
 
-// //add the chart to the code
-// function renderChart() {
-//   var ctx = document.getElementById('myChart').getContext('2d');
-//   var myChart = new Chart(ctx, {
-//     type: 'bar',
-//     data: {
-//       labels: ['R2-D2 Suitcase', 'Banana Slicer', 'iPad Toilet Paper Stand', 'Toeless Rain Boots', 'Breakfast Oven', 'Meatball Bubble Gum', 'Red Rounded Chair', 'Cthulhu', 'Dog Duckbill', 'Dragon Meat Can', 'Utensil Pen Cap', 'Pet Sweep', 'Pizza Scissors', 'Shark Sleeping Bag', 'Sweep Onesie', 'Tauntaun Sleeping Bag', 'Unicorn Meat Can', 'Tentacle USB', 'Watering Can', 'Wine Glass'],
-//       datasets: [{
-//         label: '# of Votes',
-//         data: [Product.FinalViews],
-//         backgroundColor: [
-//           'rgba(255, 99, 132, 0.2)',
-//           'rgba(54, 162, 235, 0.2)',
-//           'rgba(255, 206, 86, 0.2)',
-//           'rgba(75, 192, 192, 0.2)',
-//           'rgba(153, 102, 255, 0.2)',
-//           'rgba(255, 159, 64, 0.2)'
-//         ],
-//         borderColor: [
-//           'rgba(255,99,132,1)',
-//           'rgba(54, 162, 235, 1)',
-//           'rgba(255, 206, 86, 1)',
-//           'rgba(75, 192, 192, 1)',
-//           'rgba(153, 102, 255, 1)',
-//           'rgba(255, 159, 64, 1)'
-//         ],
-//         borderWidth: 1
-//       }]
-//     },
-//     options: {
-//       scales: {
-//         yAxes: [{
-//           ticks: {
-//             beginAtZero: true
-//           }
-//         }]
-//       }
-//     }
-//   });
-// }
+//add the chart to the code
+function renderChart() {
+  var ctx = document.getElementById('myChart').getContext('2d');
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['R2-D2 Suitcase', 'Banana Slicer', 'iPad Toilet Paper Stand', 'Toeless Rain Boots', 'Breakfast Oven', 'Meatball Bubble Gum', 'Red Rounded Chair', 'Cthulhu', 'Dog Duckbill', 'Dragon Meat Can', 'Utensil Pen Cap', 'Pet Sweep', 'Pizza Scissors', 'Shark Sleeping Bag', 'Sweep Onesie', 'Tauntaun Sleeping Bag', 'Unicorn Meat Can', 'Tentacle USB', 'Watering Can', 'Wine Glass'],
+      datasets: [{
+        label: '# of Votes',
+        data: Product.FinalViews,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  });
+}
 
 //create Event handler
 Product.container.addEventListener('click', handleClick);
